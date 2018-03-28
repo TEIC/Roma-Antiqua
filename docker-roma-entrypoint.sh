8e83c4cb24de256805a1ca2df6a443109021ad1f
Peter Stadler <stadlerpeter@yahoo.fr>
2018-03-28 13:17:53 +0200

    dynamically change base URL of roma_xquery_server

--- docker-roma-entrypoint.sh
+++ docker-roma-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i -e "s+.*define.*oxgarage_server.*+define (\'oxgarage_server\',\'${OXGARAGE_SERVER}\');+" ${WWW_HOME}/tei-roma/roma/config.php
+sed -i -e "s+.*define.*roma_xquery_server.*+define (\'roma_xquery_server\',\'${ROMA_XQUERY_SERVER}\');+" ${WWW_HOME}/tei-roma/roma/config.php
 
 # call the entrypoint script from the source image php:7-apache
 exec apache2-foreground
