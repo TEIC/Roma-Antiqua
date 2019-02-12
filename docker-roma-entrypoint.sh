e196a3b3a49f165f810e1bb8df7d779fda85dbc9
Peter Stadler <stadlerpeter@yahoo.fr>
2019-02-12 08:28:01 +0100

    add option to overwrite `roma_teiweb_server` variable

--- docker-roma-entrypoint.sh
+++ docker-roma-entrypoint.sh
@@ -2,6 +2,7 @@
 
 sed -i -e "s+.*define.*oxgarage_server.*+define (\'oxgarage_server\',\'${OXGARAGE_SERVER}\');+" ${WWW_HOME}/tei-roma/roma/config.php
 sed -i -e "s+.*define.*roma_xquery_server.*+define (\'roma_xquery_server\',\'${ROMA_XQUERY_SERVER}\');+" ${WWW_HOME}/tei-roma/roma/config.php
+sed -i -e "s+.*define.*roma_xquery_server.*+define (\'roma_teiweb_server\',\'${ROMA_TEIWEB_SERVER}\');+" ${WWW_HOME}/tei-roma/roma/config.php
 
 # call the entrypoint script from the source image php:7-apache
 exec apache2-foreground
